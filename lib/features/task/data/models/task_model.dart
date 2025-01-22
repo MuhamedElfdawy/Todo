0401b9c81c4db02f0c50832e43c005162cf38ba4
MuhamedElfdawy <m.elfdawy500@gmail.com>
2025-01-22 10:56:49 +0200

    create task model

---
+++ lib/features/task/data/models/task_model.dart
@@ -0,0 +1,39 @@
+import 'package:equatable/equatable.dart';
+
+class TaskModel extends Equatable {
+  final String id;
+  final String img;
+  final String title;
+  final String desc;
+  final String priority;
+  final String status;
+  final String user;
+  final String date;
+
+  const TaskModel(
+      {required this.id,
+        required this.img,
+        required this.title,
+        required this.desc,
+        required this.priority,
+        required this.status,
+        required this.user,
+        required this.date,
+      });
+
+  factory TaskModel.fromJson(Map<String, dynamic> json) {
+    return TaskModel(
+      id: json['_id'],
+      img: json['image'],
+      title: json['title'],
+      desc: json['desc'],
+      priority: json['priority'],
+      status: json['status'],
+      user: json['user'],
+      date: json['createdAt'],
+    );
+  }
+  @override
+  List<Object?> get props => [id,img,title,desc,priority,status,user,date];
+
+}
